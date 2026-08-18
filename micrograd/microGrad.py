18ef3ced7948b5bd1f1447c0b7d085eb1894bc47
huchentong <huchentong@outlook.com>
2026-08-18 23:00:29 +0800

    add Value definition and backward func

---
+++ micrograd/microGrad.py
@@ -0,0 +1,85 @@
+
+class Value:
+
+	def __init__(self, data, children=()):
+		self.data = data
+		self.grad = 0
+		self._prev = set(children)
+		self._backward = lambda:None
+
+	def __add__(self, other):
+		other = other if isinstance(other, Value) else Value(other)
+		out = Value(self.data + other.data, (self, other))
+
+		def _backward():
+			self.grad += out.grad
+			other.grad += out.grad
+
+		out._backward = _backward
+		return out
+
+	def __mul__(self, other):
+		other = other if isinstance(other, Value) else Value(other)
+		out = Value(self.data * other.data, (self, other))
+
+		def _backward():
+			self.grad += other.data * out.grad
+			other.grad += self.data * out.grad
+
+		out._backward = _backward
+		return out
+
+	def __pow__(self, other):
+		assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+		out = Value(self.data ** other, (self,))
+
+		def _backward():
+			self.grad += other * self.data ** (other - 1) * out.grad
+
+		out._backward = _backward
+		return out
+
+	def backward(self):
+		topo = []
+		visited = set()
+
+		def build_topo(v):
+			if v not in visited:
+				visited.add(v)
+				for child in v._prev:
+					build_topo(child)
+				topo.append(v)
+
+		build_topo(self)
+
+		self.grad = 1.
+		for v in reversed(topo):
+			v._backward()
+
+
+
+
+	def __neg__(self):
+		return self * (-1)
+	
+	def __radd__(self, other):
+		return self + other
+
+	def __sub__(self, other):
+		return self + (-other)
+
+	def __rsub__(self, other):
+		return other + (-self)
+
+	def __rmul__(self, other):
+		return self * other
+
+	def __truediv__(self, other):
+		return self * (other**-1)
+
+	def __rtruediv__(self, other):
+		return other * (self**-1)
+
+	def __repr__(self):
+		return f"Value(data={self.data}, grad= {self.grad})"
+	
